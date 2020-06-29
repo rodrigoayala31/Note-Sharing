27cfd1dcb76b637c1e9b5a290a3c476ae3d43690
Rodrigo Ayala <rodrigoayala@Rodrigos-MacBook-Air-2.local>
2020-06-29 17:37:00 -0500

    Added the submit form code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topic: "",
+      description: "",
+      collegeName: "",
+      courseName: "",
+    };
 
-export default App;
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      topic: event.target.topic,
+      description: event.target.description,
+      collegeName: event.target.collegeName,
+      courseName: event.target.courseName
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="form-wrapper">
+        <h1>Share Notes</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label for="avatar" className="chooseFile">
+              Choose an image or a PDF file:
+            </label>
+            <input type="file" id="avatar" name="avatar" accept="image/*, .pdf" className="inputFile"/>
+          </div>
+          <div>
+            <label>
+              Topic
+              <input type="text" name="topic" value={this.state.topic} onChange={this.handleChange} />
+            </label>
+          </div>
+          <div>
+            <label>
+              Description 
+              <input type="text" name="description" value={this.state.description} onChange={this.handleChange} />
+            </label>
+          </div>
+          <div>
+            <label>
+              College Name 
+              <input type="text" name="collegeName" value={this.state.collegeName} onChange={this.handleChange} />
+            </label>
+          </div>
+          <div>
+            <label>
+              Course Name  
+              <input type="text" name="courseName" value={this.state.courseName} onChange={this.handleChange} />
+            </label>
+          </div>
+          <div className="submit">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+        </div>
+      </div>
+    );
+  }
+}
+  
+  export default App;
--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -5,15 +5,15 @@ importScripts("https://www.gstatic.com/firebasejs/7.14.6/firebase-app.js");
 
 // Your Firebase configuration
 var firebaseConfig = {
-    apiKey: "RandomAPIKey",
-    authDomain: "random-86215.firebaseapp.com",
-    databaseURL: "https://random.firebaseio.com",
-    projectId: "random-86215",
-    storageBucket: "random-86215.appspot.com",
-    messagingSenderId: "8888888",
-    appId: "some::id",
-    measurementId: "G-SOMEID"
-}
+    apiKey: "AIzaSyBORqWbbMJocP5u5rNNAvXqW2bPjQygKK4",
+    authDomain: "note-sharing-app-1f607.firebaseapp.com",
+    databaseURL: "https://note-sharing-app-1f607.firebaseio.com",
+    projectId: "note-sharing-app-1f607",
+    storageBucket: "note-sharing-app-1f607.appspot.com",
+    messagingSenderId: "689331539874",
+    appId: "1:689331539874:web:de12794073c22a08512cfb",
+    measurementId: "G-0GWYYEG44Y"
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
